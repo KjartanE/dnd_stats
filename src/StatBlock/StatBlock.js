43cc5db7f985b597ce2e1af8e2bad05e19d862f1
Sam Warren <samwarren4@gmail.com>
2021-09-16 15:47:27 -0700

    simplify modifier calculator

--- src/StatBlock/StatBlock.js
+++ src/StatBlock/StatBlock.js
@@ -49,49 +49,11 @@ class StatBlock extends React.Component{
     
 
     accessScore = () => {
-        var data = this.state.score;
-        return(data);
+        return this.state.score; 
     } 
 
     modCalc = (val) =>{
-        
-        switch (val) {
-            case 1:
-                return(-5);
-            case 2:
-            case 3:
-                return(-4);
-            case 4:
-            case 5:
-                return(-3);
-            case 6:
-            case 7:
-                return(-2);
-            case 8:
-            case 9:
-                return(-1);
-            case 10:
-            case 11:          
-                return(0);
-            case 12:
-            case 13:
-                return(1);
-            case 14:
-            case 15:
-                return(2);
-            case 16:
-            case 17:
-                return(3);
-            case 18:
-            case 19:
-                return(4);
-            case 20:
-            case 21:
-                return(5);               
-            default:
-                break;
-        }
-        return;
+        return Math.floor((val-10)/2);
     }
     
     adjustStat = (val) => {
